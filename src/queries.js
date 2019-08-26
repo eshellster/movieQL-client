b9813041585e6ff3978eb48d27f4609615a78688
eshell <eshellster@gmail.com>
2019-08-26 13:09:03 +0900

    movie Home page

--- src/queries.js
+++ src/queries.js
@@ -3,9 +3,11 @@ import gql from "graphql-tag";
 export const HOME_PAGE = gql`
   query {
     movies(limit: 50, rating: 7) {
+      id
       title
       rating
       genres
+      medium_cover_image
     }
   }
 `;
